825283bfca551278517f701d5ea74acb03c9e8cb
DONGSHIH TENG <tou9702@gmail.com>
2016-06-09 00:47:40 +0900

    Fix routes.rb and Add contents in show.html

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,6 +1,6 @@
 class InfosController < ApplicationController
 
-  before_action :set_info, only: [:show,:update,:edit,:delete]
+  before_action :set_info, only: [:show,:update,:edit,:destroy]
 
   def index
     @infos = Info.all
