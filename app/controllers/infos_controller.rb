c163849f71935cc9a22fe84f11ce64b4cec2990c
DONGSHIH TENG <tou9702@gmail.com>
2016-06-08 11:26:08 +0900

    Add CRUD in infos_controller.rb and add views

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,2 +1,51 @@
 class InfosController < ApplicationController
+
+  before_action :set_info, only: [:show,:update,:edit,:delete]
+
+  def index
+    @infos = Info.all
+  end
+
+  def show
+    # got data from before_action
+  end
+
+  def new
+    @infos = Info.new
+  end
+
+  def create
+    @infos = Info.create(info_params)
+    if @infos.save
+      redirect_to info_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    # got data from before_action
+  end
+
+  def update
+    if @infos.update(info_params)
+      redirect_to info_path
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @infos.destroy
+    redirect_to info_path
+  end
+
+  private
+    def info_params
+      params.require(:info).permit(:id)
+    end
+
+    def set_info
+      @infos = Info.find(params[:id])
+    end
 end
