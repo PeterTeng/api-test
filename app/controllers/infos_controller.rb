f3254120473afdf63b85d3dcdda449fd6203db13
DONGSHIH TENG <tou9702@gmail.com>
2016-06-15 15:11:37 +0900

    Add seed data

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,38 +1,37 @@
 class InfosController < ApplicationController
 
-  before_action :set_info, only: [:show,:create,:update,:edit,:destroy]
 
   def index
     @infos = Info.all
   end
-
-  def show
-    # got data from before_action
-  end
-
-  def new
-    @info = Info.new
-  end
-
-  def create
-    @info = Info.create(info_params)
-    if @info.save
-      redirect_to info_path
-    else
-      render 'new'
-    end
-  end
-
-  def edit
-    # got data from before_action
-  end
-
-  def update
-    if @info.update(info_params)
-    else
-      render 'edit'
-    end
-  end
+  #
+  # def show
+  #   # got data from before_action
+  # end
+  #
+  # def new
+  #   @info = Info.new
+  # end
+  #
+  # def create
+  #   @info = Info.create(info_params)
+  #   if @info.save
+  #     redirect_to :back
+  #   else
+  #     render 'new'
+  #   end
+  # end
+  #
+  # def edit
+  #   # got data from before_action
+  # end
+  #
+  # def update
+  #   if @info.update(info_params)
+  #   else
+  #     render 'edit'
+  #   end
+  # end
 
   def destroy
     @info.destroy
